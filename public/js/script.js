16ee9d44346c555f8c5b62cd6ce61c5a0e5d6aef
Jonas-sh10 <jhonatansh17@gmail.com>
2023-09-07 13:03:54 -0500

    JavaScript+

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
 //aca se hace la logica del html
 // Hola
 // Adios
+
+const inputName = document.querySelector('#nombre');
+const inputApellido = document.querySelector('#apellido');
+const inputEmail = document.querySelector('#email');
+const inputCodigo = document.querySelector('#codigo');
+const inputFecha = document.querySelector('#fecha');
+const inputContrasenia = document.querySelector('#contrasenia');
+const inputRecontrasenia = document.querySelector('#recontrasenia');
+
+console.log({ inputName, inputApellido, inputEmail, inputCodigo, inputFecha, inputContrasenia, inputRecontrasenia })
